1c77925205d094835020141b33ca4b7fbe3f48e0
Hao Wu <haowu0802@gmail.com>
2017-07-09 14:40:37 -0700

    env dep settings
    env dep settings

--- hello.py
+++ hello.py
@@ -2,20 +2,26 @@
 Demonstrate how to make a single file hello world web application using Django (commonly referred as heavy weighted framework)
     run it with:    python hello.py runserver
 """
+import os  # for env var
 import sys  # for using manage.py with arguments
 from django.conf import settings  # for Django settings, settings must be set before importing other components
 
+# env dependent settings
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'  # get debug flag from env, default on
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))  # get secret_key from env, default 32 b rand
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')  # allowing all
+
 # the settings, typically in settings.py
 settings.configure(
-    DEBUG=True,  # using debug mode
-    SECRET_KEY='somekeyhere',
+    DEBUG=DEBUG,  # using debug mode
+    SECRET_KEY=SECRET_KEY,  # env dependent
+    ALLOWED_HOSTS=ALLOWED_HOSTS,  # env dep
     ROOT_URLCONF=__name__,  # point outing to the right place
     MIDDLEWARE_CLASSES=(
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ),
-    ALLOWED_HOSTS=['*'],
 )
 
 
